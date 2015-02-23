bb716beef9aba039f4fdc27be815f2877018d66a
Sergey <sergeyzsg@yandex.ru>
2015-02-23 09:09:26 +0500

    Update pingvin96.js

--- javascripts/pingvin96.js
+++ javascripts/pingvin96.js
@@ -1,4 +1,10 @@
+$(function() {
+// Begin
+
 $('.b_catalog').on('click', '.cart.select_variant', function() {
   alert('1');
   yaCounter27482889.reachGoal('Cart_Click_Product_List');
+});
+
+// End
 });
