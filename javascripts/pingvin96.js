6f075c23ef46b68e753c5f8d35e090d8906f7154
Sergey <sergeyzsg@yandex.ru>
2015-02-23 09:16:40 +0500

    Update pingvin96.js
    Положить в корзину.

--- javascripts/pingvin96.js
+++ javascripts/pingvin96.js
@@ -1,8 +1,13 @@
 $(function() {
 // Begin
 
-$('.b_catalog').on('click', '.cart.select_variant', function() {
+$('body').on('click', '.cart.select_variant', function() {
   yaCounter27482889.reachGoal('Cart_Click_Product_List');
+});
+
+$('body').on('click', '.cart:not(.select_variant)', function() {
+  alert(3);
+  yaCounter27482889.reachGoal('Drop_In_Cart');
 });
 
 // End
