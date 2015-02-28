3b0c040c2f5649a153285d73caebde51bd5993a0
Sergey <sergeyzsg@yandex.ru>
2015-02-28 10:40:17 +0500

    Написать нам

--- javascripts/pingvin96.js
+++ javascripts/pingvin96.js
@@ -9,5 +9,15 @@ $('body').on('click', '.cart:not(.select_variant)', function() {
   yaCounter27482889.reachGoal('Drop_In_Cart');
 });
 
+$('.block_info .b_pr_tabs').before(
+'<div class="form" style="padding: 0; margin: 0; padding-bottom: 10px;">'
+  '<button class="js-ask-question"><span>Задать вопрос</span></button>'
+'</div>'
+);
+
+$('.js-ask-question').click(function() {
+  window.assign('/feedback/');
+});
+
 // End
 });
